7d688774fe8235177e2b361e8f445edbae569591
Guigol <89980457+Guigol@users.noreply.github.com>
2023-01-09 22:13:48 +0100

    Commit1

---
+++ README.md
@@ -0,0 +1,7 @@
+Autheur : AA
+
+MANGO_DB & NodeJs
+
+npm init
+
+readme pas encore...
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+console.log
+(`Salut ! Salut ! ${process.env.FIRSTNAME} ${process.env.LASTNAME}, quoi de neuf ?`)
