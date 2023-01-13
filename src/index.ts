180604e33a5ef498088be18f5c2f3b0c567a8a78
Guigol <89980457+Guigol@users.noreply.github.com>
2023-01-13 16:50:29 +0100

    PIZZAS

--- src/index.ts
+++ src/index.ts
@@ -1,103 +1,23 @@
 import fastify from 'fastify'
+import fastifyPlugin from 'fastify-plugin'
+import calculatrix from './routes/calculatrix'
+import mongodb from '@fastify/mongodb'
+import pizzas from './routes/pizzas'
 
-/**
- * @module calculatrice
- *
- * @description
- *  Contient la correction de l'exercice de calculatrice.
- */
+// Création d'une application fastify
+const app = fastify({ logger: true })
 
-// Nous allons tout d'abord créer une application fastify
-const app = fastify()
-
-// Type définissant les paramètre à envoyer
-// à notre route d'addition
-type AddRoute = {
-  Params: {
-    x: string
-    y: string
-  }
-}
-
-// Route permettant de calculer 2 entiers
-app.get<AddRoute>('/add/:x/:y', request => {
-  const x = parseInt(request.params.x)
-  const y = parseInt(request.params.y)
-
-  return x + y
+// Je connécte une base de données
+app.register(mongodb, {
+  url: process.env.DATABASE_URL,
+  database: 'pizzas',
 })
 
-// Route permettant de soustraire 2 entier
-app.get<AddRoute>('/sub/:x/:y', request => {
-  const x = parseInt(request.params.x)
-  const y = parseInt(request.params.y)
+// Je connécte toutes mes routes
+app.register(fastifyPlugin(pizzas))
+app.register(fastifyPlugin(calculatrix))
 
-  return x - y
+// Démarage du serveur http
+app.listen({ port: 5353, host: '127.0.0.1' }, () => {
+  console.log("Le serveur http est prêt sur l'adresse : http://127.0.0.1:5353")
 })
-
-// Route permettant de multiplier 2 entier
-app.get<AddRoute>('/mul/:x/:y', request => {
-  const x = parseInt(request.params.x)
-  const y = parseInt(request.params.y)
-
-  return x * y
-})
-
-// Route permettant de diviser 2 entier
-app.get<AddRoute>('/div/:x/:y', (request, reply) => {
-  const x = parseInt(request.params.x)
-  const y = parseInt(request.params.y)
-
-  // Je m'assure que nous ne réalisons pas de division par 0
-  if (y === 0) {
-    reply.code(400)
-
-    return 'Division par 0 impossible !'
-  }
-
-  return x / y
-})
-
-type OperationOwner = {
-  Querystring: {
-    operation?: 'add' | 'sub' | 'div' | 'mul'
-  }
-}
-
-app.get<AddRoute & OperationOwner>('/calc/:x/:y', (request, reply) => {
-  console.table(request.headers)
-  const operation = request.query.operation || 'add'
-  const x = parseInt(request.params.x)
-  const y = parseInt(request.params.y)
-
-  if (operation === 'add') {
-    return x + y
-  }
-
-  if (operation === 'sub') {
-    return x - y
-  }
-
-  if (operation === 'mul') {
-    return x * y
-  }
-
-  if (y === 0) {
-    reply.code(400)
-
-    return 'Division par 0 impossible !'
-  }
-
-  return x / y
-})
-
-// Nous devons lancer (listen) notre serveur :
-app.listen(
-  { port: parseInt(process.env.PORT || '5353'), host: process.env.HOST },
-  () => {
-    console.log(
-      "Le serveur pour la calculatrice est prêt sur l'adresse http://127.0.0.1:5353",
-    )
-  },
-)
-
